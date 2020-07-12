8d8132315f43750b7305600a0864edae9f597fcc
Carlos F Silva <carlosfeco@gmail.com>
2020-07-12 17:52:02 -0500

    Final

--- api/api.py
+++ api/api.py
@@ -10,11 +10,18 @@ app = Flask(__name__)
 api = Api(
     app, 
     version='1.0', 
-    title='Car Price Prediction API',
-    description='Car Price Prediction API')
+    title='Movie Genre Prediction API',
+    description='''Based on movie year, title and plot, this API predicts the genres of the movie
+        Universidad de Los Andes
+        Master in Analytic Intelligence for Decision Making
+        Advanced Methods in Data Analysis
+        
+        Carlos Francisco Silva Ortiz - 201920463
+        Diana Rocío Díaz Rodríguez - 201331684
+        Javier Alfonso Lesmes Patiño - 200820243''')
 
-ns = api.namespace('Car Price Prediction', 
-     description='Car Price Prediction')
+ns = api.namespace('Movie Genres Prediction', 
+     description='Predicts the genres of the movie')
    
 parser = api.parser()
 
@@ -22,37 +29,22 @@ parser.add_argument(
     'Year', 
     type=int, 
     required=True, 
-    help='Year model', 
+    help='Movie Year', 
     location='args')
     
 parser.add_argument(
-    'Mileage', 
-    type=int, 
+    'Title', 
+    type=str, 
     required=True, 
-    help='Car Mileage', 
+    help='Title of the movie', 
     location='args')
 
 parser.add_argument(
-    'State', 
+    'Plot', 
     type=str, 
     required=True, 
-    help='State', 
+    help='Plot fo the movie', 
     location='args')
-
-parser.add_argument(
-    'Make', 
-    type=str, 
-    required=True, 
-    help='Make', 
-    location='args')
-
-parser.add_argument(
-    'Model', 
-    type=str, 
-    required=True, 
-    help='Model', 
-    location='args')   
-    
 
 resource_fields = api.model('Resource', {
     'result': fields.String,
@@ -67,7 +59,7 @@ class PriceForecast (Resource):
         args = parser.parse_args()
         
         return {
-         "result": predict_proba(args['Year'],args['Mileage'],args['State'],args['Make'],args['Model'])
+         "result": predict_proba(args['Year'],args['Title'],args['Plot'])
         }, 200
     
     
